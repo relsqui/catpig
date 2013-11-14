1b40dad5592057c60ea739176273e89cb750f2bb
Finn Ellis <finnre@cat.pdx.edu>
2013-11-14 03:27:04 -0800

    Look for .printers files in the pwd, added --alerts

--- catpig.py
+++ catpig.py
@@ -2,15 +2,7 @@
 
 import cups, sys, argparse
 from pprint import pprint
-
-
-printer_lists = [
-    'fab.printers',
-    'eb.printers',
-    'cat.printers',
-]
-
-confirmations = ['y', 'yes']
+from glob import glob
 
 
 conn = cups.Connection()
@@ -18,14 +10,21 @@ all_printers = conn.getPrinters()
 
 check_printers = []
 printers_by_file = {}
+printer_lists = [p[2:] for p in glob("./*.printers")]
 for list_file in printer_lists:
     with open(list_file) as fp:
         printers_by_file[list_file] = [p.strip() for p in fp.readlines()]
         check_printers.extend(printers_by_file[list_file])
 
-parser = argparse.ArgumentParser(description="Printer Information Generator. Get information on available printers and send test prints.")
-parser.add_argument("printer", metavar="PRINTER", choices=check_printers, nargs="?", help="ID of printer (if absent, PIG will print a summary)")
+parser = argparse.ArgumentParser(description="""
+CAT Printer Information Generator. Get information on available printers
+and send test prints. To add printers to catpig's checklist, list their IDs
+in files whose names end in '.printers'.  The IDs will be grouped by file
+in the summary view.
+""")
+parser.add_argument("printer", metavar="PRINTER", choices=check_printers, nargs="?", help="ID of printer (if absent, catpig will print a summary)")
 parser.add_argument("-t", "--test", action='store_true', help="send a test page to the selected printer")
+parser.add_argument("-a", "--alerts", action='store_true', help="summarize only the printers which have alerts")
 args = parser.parse_args()
 
 
@@ -33,7 +32,7 @@ if args.printer:
     try:
         printer = all_printers[args.printer]
     except KeyError:
-        print "Sorry, can't reach {}. Run PIG with no arguments to get a printer list.".format(args.printer)
+        print "Sorry, can't reach {}. Run catpig with no arguments to get a printer list.".format(args.printer)
         sys.exit(1)
     print "Location:\t{}".format(printer['printer-location'])
     print "Model:\t\t{}".format(printer['printer-make-and-model'])
@@ -44,6 +43,7 @@ if args.printer:
         for reason in printer['printer-state-reasons']:
             print "\t\t{}".format(reason)
     if args.test:
+        confirmations = ['y', 'yes']
         print
         confirm = raw_input("Sending test page. Confirm? ")
         if confirm in confirmations:
@@ -57,12 +57,21 @@ else:
         print filename.upper()
         for printer_name in printers_by_file[filename]:
             if printer_name not in all_printers.keys():
-                print "XX  {}\tNOT FOUND".format(printer_name)
+                print "XX {}\tNOT FOUND".format(printer_name)
                 continue
             printer = all_printers[printer_name]
             if printer['printer-state-reasons'][0] != "none":
-                print "!! ",
+                has_messages = True
+                prefix = "!! "
             else:
-                print "   ",
-            print "{}\t{}".format(printer_name, printer['printer-location'])
+                has_messages = False
+                prefix = "   "
+            if args.alerts:
+                if not has_messages:
+                    continue
+                prefix = ""
+                info = ", ".join(printer['printer-state-reasons'])
+            else:
+                info = printer['printer-location']
+            print "{}{}\t{}".format(prefix, printer_name, info)
         print
